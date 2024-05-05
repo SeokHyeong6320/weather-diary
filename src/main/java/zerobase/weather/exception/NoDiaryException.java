065dce9b43c8f464a9694a7edfbd6bee8ee6e9e8
SeokHyeong6320 <hsh9171@icloud.com>
2024-05-05 23:31:40 +0900

    exception

--- src/main/java/zerobase/weather/exception/NoDiaryException.java
+++ src/main/java/zerobase/weather/exception/NoDiaryException.java
@@ -1,8 +1,5 @@
 package zerobase.weather.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class NoDiaryException extends RuntimeException {
 
     public NoDiaryException(String message) {
